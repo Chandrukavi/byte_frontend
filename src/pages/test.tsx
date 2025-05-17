74525fa21c3ec0a7afb29ff7ce1be78d3b6d0b56
ChandruKavi <chandrukavi1604@gmail.com>
2025-05-17 10:31:51 +0530

    Fix

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-2xl">
+        Tailwind CSS v4 is working! 🎉
+    </div>
+  );
+}
+
+export default Test;
